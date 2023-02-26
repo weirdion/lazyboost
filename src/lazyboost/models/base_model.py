06a1b0ca2fdd59c58963b15ee367f6a9c6ad4272
Ankit Sadana <sadana.ankit@gmail.com>
2023-02-25 21:20:44 -0500

    [#1] Add Etsy order model

---
+++ src/lazyboost/models/base_model.py
@@ -0,0 +1,28 @@
+#   LazyBoost
+#   Copyright (C) 2023  Ankit Sadana
+#  #
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#  #
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#  #
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+class BaseModel(object):
+    def __init__(self, data):
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)):
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return BaseModel(value) if isinstance(value, dict) else value
