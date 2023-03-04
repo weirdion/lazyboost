7e000c60b0f20769d1016026a806a508acf4a5c0
Ankit Sadana <sadana.ankit@gmail.com>
2023-03-04 14:13:49 -0500

    🧱 Added Secrets Manager. 🔥 Killing dotenv

--- infra/lib/lazyboost-stack.ts
+++ infra/lib/lazyboost-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, RemovalPolicy, SecretValue } from 'aws-cdk-lib';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LazyboostStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LazyboostQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const secret = new secretsmanager.Secret(this, 'LAZYBOOST_CREDS', {
+      secretName: 'LAZYBOOST_CREDS',
+      removalPolicy: RemovalPolicy.RETAIN,
+      });
+    new CfnOutput(this, 'LazyBoostCredsArn', { value: secret.secretArn });
   }
 }
