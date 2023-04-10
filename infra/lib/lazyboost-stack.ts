9829f27b8a237fd2f535ddb8918668b97ba654f6
Ankit Sadana <sadana.ankit@gmail.com>
2023-04-09 21:43:56 -0400

    [#12] Add API GW for redirect request handling

--- infra/lib/lazyboost-stack.ts
+++ infra/lib/lazyboost-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Duration, RemovalPolicy } from 'aws-cdk-lib';
+import { Integration, LambdaIntegration, LambdaRestApi, RestApi } from 'aws-cdk-lib/aws-apigateway';
 import { RuleTargetInput } from 'aws-cdk-lib/aws-events';
-import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
+import { AssetCode, Code, DockerImageCode, DockerImageFunction, Function, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
 import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 import path = require('path');
@@ -14,6 +15,42 @@ export class LazyboostStack extends cdk.Stack {
       secretName: 'LAZYBOOST_CREDS',
       removalPolicy: RemovalPolicy.RETAIN,
     });
+
+    const initEtsyOauth = new Function(this, 'InitEtsyOauth', {
+      functionName: 'InitEtsyOauth',
+      runtime: Runtime.NODEJS_18_X,
+      code: new AssetCode('infra/resources/lambdas/etsy-auth-init'),
+      handler: 'index.handler',
+      environment: {
+        'SECRET_NAME': secret.secretName
+      }
+    });
+    secret.grantRead(initEtsyOauth);
+    secret.grantWrite(initEtsyOauth);
+
+    const oauthRedirect = new Function(this, 'EtsyOauthRedirect', {
+      functionName: 'EtsyOauthRedirect',
+      runtime: Runtime.NODEJS_18_X,
+      code: new AssetCode('infra/resources/lambdas/etsy-auth-redirect'),
+      handler: 'index.handler',
+      environment: {
+        'SECRET_NAME': secret.secretName
+      }
+    });
+    secret.grantRead(oauthRedirect);
+    secret.grantWrite(oauthRedirect);
+
+    const apiGW = new RestApi(this, 'LazyBoostApi', {
+      deploy: false
+    });
+    apiGW.root.addMethod('ANY');
+
+    const apiNode = apiGW.root.addResource('api');
+    const authCallback = apiNode.addResource('redirect');
+    authCallback.addMethod(
+      'GET',
+      new LambdaIntegration(oauthRedirect)
+    );
 
     const lazyboost_lambda = new DockerImageFunction(this, 'LazyBoostFunction', {
       functionName: 'LazyBoostFunction',
