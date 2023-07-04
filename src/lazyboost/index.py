13be4dcb5aaf99f04bd4da9126f347b3ac3a6fe7
Ankit Sadana <sadana.ankit@gmail.com>
2023-07-04 11:46:09 -0400

    Remove custom metrics

--- src/lazyboost/index.py
+++ src/lazyboost/index.py
@@ -16,40 +16,31 @@
 #
 
 from aws_lambda_powertools import Logger
-from aws_lambda_powertools.metrics import MetricUnit, EphemeralMetrics
 
 from lazyboost.handlers import OrderHandler, OrdersEnum, ReviewHandler
 
 logger = Logger()
-metrics = EphemeralMetrics()
 
 
 def handler(event, context):
     """
     Handler function, entry point for the lambda triggers
     """
-    try:
-        logger.info(f"Starting lambda with event", event=event)
+    logger.info(f"Starting lambda with event", event=event)
 
-        if not (
+    if not (
             isinstance(event, dict)
             and "task" in event.keys()
             and event["task"] in ["order_sync", "review_sync", "sync"]
-        ):
-            raise ValueError(f"Invalid event or task type used: {event}, exiting...")
+    ):
+        raise ValueError(f"Invalid event or task type used: {event}, exiting...")
 
-        if event["task"] == "order_sync":
-            OrderHandler(order_sync_type=OrdersEnum.SYNC)
-        elif event["task"] == "review_sync":
-            ReviewHandler()
-        elif event["task"] == "sync":
-            OrderHandler(order_sync_type=OrdersEnum.SYNC)
-            ReviewHandler()
-        else:
-            raise ValueError(f"Invalid task type received: {event['task']}")
-    except Exception as e:
-        metrics.add_metric(name="LazyBoostSyncFail", unit=MetricUnit.Count, value=1)
-
-        logger.error(e)
-    finally:
-        metrics.flush_metrics(raise_on_empty_metrics=False)
+    if event["task"] == "order_sync":
+        OrderHandler(order_sync_type=OrdersEnum.SYNC)
+    elif event["task"] == "review_sync":
+        ReviewHandler()
+    elif event["task"] == "sync":
+        OrderHandler(order_sync_type=OrdersEnum.SYNC)
+        ReviewHandler()
+    else:
+        raise ValueError(f"Invalid task type received: {event['task']}")
