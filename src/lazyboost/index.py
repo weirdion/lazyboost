f5946dc61422118ecfdbcab9a81069579b8b9d74
Ankit Sadana <sadana.ankit@gmail.com>
2023-08-25 09:50:28 -0400

    [#27] Replace metric alarm with SNS publish

--- src/lazyboost/index.py
+++ src/lazyboost/index.py
@@ -14,7 +14,10 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+import os
+from datetime import datetime
 
+import boto3
 from aws_lambda_powertools import Logger
 
 from lazyboost.handlers import OrderHandler, OrdersEnum, ReviewHandler
@@ -28,19 +31,37 @@ def handler(event, context):
     """
     logger.info(f"Starting lambda with event", event=event)
 
-    if not (
-        isinstance(event, dict)
-        and "task" in event.keys()
-        and event["task"] in ["order_sync", "review_sync", "sync"]
-    ):
-        raise ValueError(f"Invalid event or task type used: {event}, exiting...")
+    try:
+        if not (
+            isinstance(event, dict)
+            and "task" in event.keys()
+            and event["task"] in ["order_sync", "review_sync", "sync"]
+        ):
+            raise ValueError(f"Invalid event or task type used: {event}, exiting...")
 
-    if event["task"] == "order_sync":
-        OrderHandler(order_sync_type=OrdersEnum.SYNC)
-    elif event["task"] == "review_sync":
-        ReviewHandler()
-    elif event["task"] == "sync":
-        OrderHandler(order_sync_type=OrdersEnum.SYNC)
-        ReviewHandler()
-    else:
-        raise ValueError(f"Invalid task type received: {event['task']}")
+        if event["task"] == "order_sync":
+            OrderHandler(order_sync_type=OrdersEnum.SYNC)
+        elif event["task"] == "review_sync":
+            ReviewHandler()
+        elif event["task"] == "sync":
+            OrderHandler(order_sync_type=OrdersEnum.SYNC)
+            ReviewHandler()
+        else:
+            raise ValueError(f"Invalid task type received: {event['task']}")
+    except Exception as e:
+        sns_topic_arn = os.getenv("SNS_ERROR_TOPIC")
+        date_now = datetime.now().strftime("%m/%d/%Y, %-I:%M:%S %p")
+        if sns_topic_arn:
+            sns_client = boto3.client("sns")
+            response = sns_client.publish(
+                TopicArn=sns_topic_arn,
+                Message=f"""
+                An error occurred during execution of LazyBoost at {date_now}.\n\n
+                Error: `{e}`\n
+                ```{e.__traceback__}```
+                """,
+                Subject=f"LazyBoost encountered an error at {date_now}",
+            )
+            logger.info(f"SNS response: {response}")
+        else:
+            raise e
